9ff9b365ff52be123d64c04d80dcff9c5513f2dc
GaraninNV <nikolas-ua@mail.ru>
2022-04-24 00:18:51 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println("Garanin Nikolay");
+        System.out.println("08.11.1986");
+    }
+}
