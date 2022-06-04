ed40c56e7f73ca71642a3c972a03a389943862da
GaraninNV <nikolas-ua@mail.ru>
2022-06-04 17:36:45 +0300

    6.0.1. Объявление массива. [#3613]

---
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,9 @@
+package ru.job4j.array;
+
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] arrayShort = new short[10];
+        String[] arrayString = new String[100500];
+        float[] arrayFloat = new float[40];
+    }
+}
