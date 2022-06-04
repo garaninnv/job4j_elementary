9e86698ea140b760e29bbc700bed943c24f2987d
GaraninNV <nikolas-ua@mail.ru>
2022-06-04 21:44:51 +0300

    6.0.1.1. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -3,7 +3,10 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] arrayShort = new short[10];
+        System.out.println("Размер массива : " + arrayShort.length);
         String[] arrayString = new String[100500];
+        System.out.println("Размер массива : " + arrayString.length);
         float[] arrayFloat = new float[40];
+        System.out.println("Размер массива : " + arrayFloat.length);
     }
 }
