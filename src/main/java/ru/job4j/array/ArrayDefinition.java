d8b1ce487d01230dfe0a229da1bb88e775f3490a
GaraninNV <nikolas-ua@mail.ru>
2022-06-04 23:36:21 +0300

    6.0.1. Объявление массива. [#3613 #308971]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,19 +2,11 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] arrayShort = new short[10];
-        System.out.println("Размер массива : " + arrayShort.length);
-        String[] arrayString = new String[100500];
-        System.out.println("Размер массива : " + arrayString.length);
-        float[] arrayFloat = new float[40];
-        System.out.println("Размер массива : " + arrayFloat.length);
-        String[] names = new String[4];
-        names[0] = "Garanin Nikolay";
-        names[1] = "Garanina Oksana";
-        names[2] = "Garanin Semen";
-        names[3] = "Garanina Anastasiya";
-        for (String element : names) {
-            System.out.println(element);
-        }
+        short[] ages = new short[10];
+        System.out.println("Размер массива : " + ages.length);
+        String[] surnames = new String[100500];
+        System.out.println("Размер массива : " + surnames.length);
+        float[] prices = new float[40];
+        System.out.println("Размер массива : " + prices.length);
     }
 }
